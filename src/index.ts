a1461ca4a7d9e60717be718ac994e86b2eed40a4
Stalin <stalindurjo@gmail.com>
2024-10-13 20:39:06 +0600

    database import updated

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import '@testConfig';
 import { initServer } from './server/server';
 import '../actions/register';
 import '../mocker/register';
-import { initializeDatabase } from './service/local-database';
+import { initializeDatabase } from './service/database';
 import { initializeMockData } from './service/mocker';
 import { routeManager } from './server/route/route-manager';
 import { mockRegistry } from './service/mocker-request/mocker-registry';
