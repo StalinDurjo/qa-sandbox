1c9899068910295f4eeebc613d59415f9b4f806e
Stalin <stalindurjo@gmail.com>
2024-10-13 20:42:04 +0600

    database import updated & code refactored

--- mocker/project/dokan/index.ts
+++ mocker/project/dokan/index.ts
@@ -1,14 +1,24 @@
+import { database } from '@src/service/database';
 import MockerProject from '@src/service/mocker-request/mocker-project';
 
 const project = new MockerProject({ projectName: 'dokan' });
 
-project.addMethod(
-  {
-    endpoint: '/generate-vendor-data'
-  },
-  async (data) => {
-    return data;
-  }
-);
+project.addMethod({ endpoint: '/vendor-store' }, async (data) => {
+  const mocker = data.mocker;
+  const counter = await database.incrementCount();
+
+  return {
+    firstName: 'Vendor',
+    lastName: `Shop${counter}`,
+    email: `vendor${counter}@mailinator.com`,
+    password: '01Dokan01!',
+    storeName: `vendor${counter}shop`,
+    phoneNumber: await mocker.person.phoneNumber(),
+    country: await mocker.location.countryName(),
+    state: await mocker.location.subdivision(),
+    postcode: await mocker.location.postcode(),
+    address: await mocker.location.streetAddress()
+  };
+});
 
 export default project;
