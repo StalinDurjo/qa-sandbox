e6cd67b2e9992b7b21b1695563524065a24b8cdb
Stalin <stalindurjo@gmail.com>
2024-10-18 10:53:30 +0600

    initial code added

--- actions/project/dokan/index.ts
+++ actions/project/dokan/index.ts
@@ -1,1 +1,23 @@
-export default class DokanActionProject {}
+import WpLoginPage from '@pages/woocommerce/wp-admin/auth/login.page';
+import { Page } from 'playwright';
+
+export default class DokanActionProject {
+  async createVendor(page: Page, { username, password }) {
+    const loginPage = new WpLoginPage(page);
+    await page.goto('http://dokan.test/my-account/');
+    await loginPage.enterUsername(username);
+    await loginPage.enterPassword(password);
+    await loginPage.clickOnLogin();
+
+    // check im vendor
+    // enter email
+    // enter password
+    // enter firstname
+    // enter lastname
+    // enter shopname
+    // enter phonenumber
+    // click on register
+
+    //
+  }
+}
