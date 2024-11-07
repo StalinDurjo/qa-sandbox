f07ec935716ff7f02239436dcb33f6cbdc255c98
Stalin <stalindurjo@gmail.com>
2024-11-07 10:14:34 +0600

    setActionProjectUrl method added

--- src/server/controller/action-controller.ts
+++ src/server/controller/action-controller.ts
@@ -1,25 +1,29 @@
 import BrowserUtil from '@src/lib/browser';
 import Action from '@src/service/action/action';
 import ActionScriptLoader from '@src/service/action/script-loader';
+import { database } from '@src/service/database';
 import { Request, Response } from 'express';
 import { request } from 'playwright';
 
 export const runSingleAction = async (req: Request, res: Response) => {
   try {
-    const { project, actionName, repeat, actionType } = req.body;
     const actionScript = new ActionScriptLoader();
     const action = new Action();
 
-    const repeatAction = repeat ? repeat : actionScript.loadScript({ project, actionName })[0]['repeat'];
+    const requestBody = req.body;
+
+    const repeatAction = requestBody['repeat']
+      ? requestBody['repeat']
+      : actionScript.loadScript({ project: requestBody['project'], actionName: requestBody['actionName'] })[0]['repeat'];
 
     for (let i = 0; i < repeatAction; i++) {
-      if (actionType === 'UI') {
+      if (requestBody['actionType'] === 'UI') {
         const browser = new BrowserUtil();
         const page = await browser.createInstance();
-        await action.run(page, { project, actionName });
+        await action.run(page, { ...requestBody });
         await browser.closeInstance();
-      } else if (actionType === 'API') {
-        await action.run(request, { project, actionName });
+      } else if (requestBody['actionType'] === 'API') {
+        await action.run(request, { ...requestBody });
       }
     }
 
@@ -34,11 +38,11 @@ export const runSingleAction = async (req: Request, res: Response) => {
 
 export const runMultipleAction = async (req: Request, res: Response) => {
   try {
-    const actionList = req.body;
+    const requestBody = req.body;
     const actionScript = new ActionScriptLoader();
     const action = new Action();
 
-    for (const _action of actionList) {
+    for (const _action of requestBody) {
       const project = _action['project'];
       const actionType = _action['actionType'];
       const actionName = _action['actionName'];
@@ -62,6 +66,33 @@ export const runMultipleAction = async (req: Request, res: Response) => {
   } catch (error) {
     res.status(400).send({
       message: 'Something went wrong while running action.',
+      error: error
+    });
+  }
+};
+
+export const setActionProjectUrl = async (req: Request, res: Response) => {
+  try {
+    const { actionProjectBaseUrl } = req.body;
+
+    await database.run(`
+      INSERT INTO options (action_project_base_url)
+      SELECT 'testvalue'
+      WHERE NOT EXISTS (
+        SELECT 1 FROM options
+      );
+    `);
+
+    await database.run(`
+      UPDATE options
+      SET action_project_base_url = '${actionProjectBaseUrl}'
+      WHERE rowid = (SELECT rowid FROM options ORDER BY rowid LIMIT 1);
+    `);
+
+    res.status(200).send('Action project base URL updated successfully!');
+  } catch (error) {
+    res.status(400).send({
+      message: 'Something went wrong while updating action project base URL.',
       error: error
     });
   }
