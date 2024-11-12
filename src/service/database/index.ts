5e63e83a67e15781385fbe3363950a6df2cf4aad
Stalin <stalindurjo@gmail.com>
2024-11-12 19:52:36 +0600

    fixed out directory not created on project start

--- src/service/database/index.ts
+++ src/service/database/index.ts
@@ -1,4 +1,4 @@
-import { createFileSync } from '@src/lib/util/file';
+import { createDirectorySync, createFileSync } from '@src/lib/util/file';
 import Database from './database';
 import { LOCAL_DATABASE } from '@src/constant';
 
@@ -6,6 +6,7 @@ export const database = new Database();
 
 export const initializeDatabase = async () => {
   // create database if it does not exist
+  createDirectorySync(process.cwd() + '/out/');
   createFileSync(LOCAL_DATABASE, process.cwd() + '/out/');
 
   // create database tables
