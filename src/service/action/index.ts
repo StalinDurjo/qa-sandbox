65bc1734938a50008d9b511967d5132de38f317a
Stalin <stalindurjo@gmail.com>
2024-11-12 22:34:38 +0600

    action feature import updated

---
+++ src/service/action/index.ts
@@ -0,0 +1,13 @@
+import fs from "fs";
+import {actionRegistry} from "@src/service/action/action-registry";
+
+const registryPath: string = process.cwd() + '/includes/actions/project'
+const files = fs.readdirSync(registryPath);
+
+for(const file of files){
+  const projectName = file
+  actionRegistry.register({
+    name: projectName,
+    directory: './' + projectName
+  });
+}
