a775360356d3f06dda5f0198c655aaee74b5a79a
Stalin <stalindurjo@gmail.com>
2024-11-13 22:31:47 +0600

    index file imports updated

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
 import '@testConfig';
 import { initServer } from './server/server';
-import './service/action'
-import '../includes/mocker/register';
+import './service/action';
+import './service/mocker';
 import { initializeDatabase } from './service/database';
-import { initializeMockData } from './service/mocker';
 import { dynamicRouter } from './server/route/dynamic-router';
-import { mockerRegistry } from './service/mocker-request/mocker-registry';
 import './service/mailer/mailer';
 import './scheduler';
 import { versionTracker } from './service/version-tracker';
+import { mockRequest } from './service/mock-request';
 
 (async () => {
   // start local server
   await initServer();
   // create database and its tables
   await initializeDatabase();
-  // populate database with mock data
-  await initializeMockData();
-  // initialize mocker projects
-  await mockerRegistry.initialize();
+  await mockRequest.initializeRoutes();
   // initialize the creation of dynamic routes
   dynamicRouter.initialize();
-
   await versionTracker.initialize();
 })();
