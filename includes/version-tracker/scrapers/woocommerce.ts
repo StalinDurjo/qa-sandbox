051ae220ff1cebe90446465ef4faa23406e33016
Stalin <stalindurjo@gmail.com>
2024-11-19 09:51:43 +0600

    usability refactor 3

--- includes/version-tracker/scrapers/woocommerce.ts
+++ includes/version-tracker/scrapers/woocommerce.ts
@@ -2,7 +2,6 @@ import { DependencyVersion } from '@src/service/version-tracker/scraper-registry
 
 export default async function woocommerceScraper({ page, targetUrl }): Promise<DependencyVersion> {
   await page.goto(targetUrl);
-
   const [pluginVersion] = await page.locator(`//aside//div[@class="wccom-product-box"]//a/div/p[text()="Latest version"]/following-sibling::p`).allInnerTexts();
 
   return {
